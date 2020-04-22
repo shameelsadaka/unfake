faa33acea7dc2b2a8126ee4a27fc1b5cd92a1b82
Shameel Kadannamanna <shameelsadaka@gmail.com>
2020-04-23 01:49:40 +0530

    Updated Bottom Navigation Bar

--- lib/screens/components/BottomNavBar.dart
+++ lib/screens/components/BottomNavBar.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 
 
 // Components
-import 'BottomNavBarItem.dart';
-
 class BottomNavBar extends StatelessWidget {
   BottomNavBar({
     Key key,
@@ -12,11 +10,12 @@ class BottomNavBar extends StatelessWidget {
 
   
 
-  final List<BottomItem> bottomIcons = [
-    BottomItem('/home',Icons.home),
-    BottomItem('/myposts',Icons.speaker_notes),
-    BottomItem('/saved_posts',Icons.bookmark),
-    BottomItem('/settings',Icons.settings),
+  final List<BottomNavItem> bottomIcons = [
+    BottomNavItem('/home',Icons.home),
+    BottomNavItem('/myposts',Icons.speaker_notes),
+    null,
+    BottomNavItem('/saved_posts',Icons.bookmark),
+    BottomNavItem('/settings',Icons.settings),
   ];
 
   @override
@@ -24,9 +23,9 @@ class BottomNavBar extends StatelessWidget {
     return BottomAppBar(
       shape: const CircularNotchedRectangle(),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: bottomIcons.map((item){
-          return BottomNavBarItem(icon:item.icon,isActive: ModalRoute.of(context).settings.name == item.routeName);
+          return item == null?new SizedBox(width: 50):BottomNavBarItemUI(icon:item.icon,isActive: ModalRoute.of(context).settings.name == item.routeName);
         }).toList(),
       ),
     );
@@ -34,9 +33,38 @@ class BottomNavBar extends StatelessWidget {
 }
 
 
-class BottomItem{
+class BottomNavItem{
   String routeName;
   IconData icon;
 
-  BottomItem(this.routeName,this.icon);
+  BottomNavItem(this.routeName,this.icon);
+}
+
+
+
+
+class BottomNavBarItemUI extends StatelessWidget {
+
+  bool isActive;
+  IconData icon;
+  
+  BottomNavBarItemUI({
+    Key key,
+    this.isActive =false,
+    this.icon
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: null,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 0,vertical:20.0), 
+        child: Icon(
+          icon,
+          color: isActive?Theme.of(context).primaryColor:Colors.black,
+        ),
+      ),
+    );
+  }
 }
