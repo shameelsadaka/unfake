b2c7ac2f2f9a90537bd81eb5779eda60a805b31d
Shameel Kadannamanna <shameelsadaka@gmail.com>
2020-04-25 04:05:03 +0530

    small renaming

--- lib/screens/setupProfilePage.dart
+++ lib/screens/setupProfilePage.dart
@@ -2,13 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:itstrue/data/logic/firebase.dart';
 
 class SetupProfilePage extends StatelessWidget {
-
-
-
-
-final _auth = FireBaseUser();
-final name = TextEditingController();
-final designation = TextEditingController();
+  final _auth = FireBaseUser();
+  final name = TextEditingController();
+  final designation = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -26,24 +22,24 @@ final designation = TextEditingController();
                         color: Colors.black),
                   ),
                   SizedBox(height: 60),
-
-
-
                   Container(
                     decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(15),
-                        boxShadow: [BoxShadow(color: Colors.black12,blurRadius: 3,offset:Offset(0,3) )]
-                    ),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 3,
+                              offset: Offset(0, 3))
+                        ]),
                     child: Column(
                       children: <Widget>[
                         nameField(),
-                        Divider(color: Colors.black26,height: 3),
+                        Divider(color: Colors.black26, height: 3),
                         designationField(),
                       ],
                     ),
                   ),
-
                   SizedBox(height: 20),
                   RaisedButton(
                     onPressed: () {
@@ -51,27 +47,20 @@ final designation = TextEditingController();
                     },
                     color: Theme.of(context).primaryColor,
                     textColor: Colors.white,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                    child: const Text(
-                        'Done',
-                        style: TextStyle(fontSize: 15)
-                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    child: const Text('Done', style: TextStyle(fontSize: 15)),
                   ),
-
-
-
-
-                ]
-            )
-        ),
+                ])),
       ),
     );
   }
+
   Container nameField() {
     return Container(
         margin: EdgeInsets.symmetric(horizontal: 5, vertical: 3),
         child: TextField(
-          controller: name,
+            controller: name,
             style: TextStyle(
               fontSize: 14.0,
             ),
@@ -80,25 +69,23 @@ final designation = TextEditingController();
               border: InputBorder.none,
               prefixIcon: Icon(Icons.person, color: Colors.black, size: 18),
               hintText: 'Full Name',
-            )
-        )
-    );
+            )));
   }
+
   Container designationField() {
     return Container(
         margin: EdgeInsets.symmetric(horizontal: 5, vertical: 3),
         child: TextField(
-          controller: designation,
+            controller: designation,
             style: TextStyle(
               fontSize: 14.0,
             ),
             textAlignVertical: TextAlignVertical.center,
             decoration: InputDecoration(
               border: InputBorder.none,
-              prefixIcon: Icon(Icons.description, color: Colors.black, size: 18),
+              prefixIcon:
+                  Icon(Icons.description, color: Colors.black, size: 18),
               hintText: 'Enter your Designation',
-            )
-        )
-    );
+            )));
   }
 }
